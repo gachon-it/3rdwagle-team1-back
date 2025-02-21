5d6b97c3c94eebf42c1df6c49a2ca1e5abf88ce3
huncozyboy <leejh5838@naver.com>
2025-02-21 15:38:07 +0900

    feat : 전역적으로 사용할 응답 형태 추가

---
+++ src/main/java/com/example/yoURL/global/common/response/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.example.yoURL.global.common.response;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+
+    private Integer code;
+    private String message;
+    private T result;
+
+    public static <T> ApiResponse<T> response(int code, String message, T data) {
+        return new ApiResponse<>(code, message, data);
+    }
+
+    public static <T> ApiResponse<T> response(int code, String message) {
+        return new ApiResponse<>(code, message, null);
+    }
+}
