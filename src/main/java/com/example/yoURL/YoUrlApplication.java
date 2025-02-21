99a37d3b4d23cf88f4627a4c05a7c96245d1cc27
huncozyboy <leejh5838@naver.com>
2025-02-21 14:43:59 +0900

    chore: initial commit

---
+++ src/main/java/com/example/yoURL/YoUrlApplication.java
@@ -0,0 +1,13 @@
+package com.example.yoURL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yoURL/YoUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yoURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
