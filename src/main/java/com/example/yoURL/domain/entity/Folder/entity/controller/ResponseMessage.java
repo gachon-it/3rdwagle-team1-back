9d5ed1dc1b8c10276e75907ad206b18ab25aa505
huncozyboy <leejh5838@naver.com>
2025-02-22 00:53:17 +0900

    feat : 성공 메세지 구현

--- src/main/java/com/example/yoURL/domain/entity/Folder/entity/controller/ResponseMessage.java
+++ src/main/java/com/example/yoURL/domain/entity/Folder/entity/controller/ResponseMessage.java
@@ -9,8 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum ResponseMessage {
     CREATE_SUCCESS(HttpStatus.CREATED.value(), "폴더 생성 성공했습니다."),
     UPDATE_SUCCESS(HttpStatus.OK.value(), "폴더 수정 성공했습니다."),
-    DELETE_SUCCESS(HttpStatus.OK.value(), "폴더 삭제 성공했습니다");
+    DELETE_SUCCESS(HttpStatus.OK.value(), "폴더 삭제 성공했습니다"),
 
+    FOLDER_LIKE_SUCCESS(HttpStatus.OK.value(), "폴더 즐겨찾기 등록에 성공했습니다."),
+    FOLDER_LIKE_REMOVE_SUCCESS(HttpStatus.OK.value(), "폴더 즐겨찾기 해제에 성공했습니다.");
 
     private final int code;
     private final String message;
