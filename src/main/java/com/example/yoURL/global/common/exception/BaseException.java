ba0bf5fccb5fa39931e9e321d017f7e84e18940e
huncozyboy <leejh5838@naver.com>
2025-02-21 15:37:10 +0900

    feat : 전역적으로 사용할 예외처리 구현

---
+++ src/main/java/com/example/yoURL/global/common/exception/BaseException.java
@@ -0,0 +1,15 @@
+package com.example.yoURL.global.common.exception;
+
+public class BaseException extends RuntimeException {
+
+    private final int errorCode;
+
+    public BaseException(int errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+}
---
+++ src/main/java/com/example/yoURL/global/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.yoURL.global.common.exception;
+
+import com.example.yoURL.global.common.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final int SERVER_ERROR_CODE = 500;
+    private static final String SERVER_ERROR_MESSAGE = "에러가 발생했습니다.";
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ApiResponse<Void>> handleBaseException(BaseException e) {
+        ApiResponse<Void> response = ApiResponse.response(e.getErrorCode(), e.getMessage(), null);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException() {
+        ApiResponse<Void> response = ApiResponse.response(SERVER_ERROR_CODE, SERVER_ERROR_MESSAGE, null);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+}
